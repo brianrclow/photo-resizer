c9420fe3a60b37a64cc8838e58668c2fe615f8a7
Brian Clow <brianclow@u.boisestate.edu>
2020-08-31 14:09:53 -0600

    Updated function with more dependencies

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 /*
@@ -14,8 +17,29 @@ import (
 	This has been adapted to test Go and also to use with AWS Lambda.
 */
 func main() {
+	lambda.Start(HandleRequest)
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+    fmt.Fprintf(os.Stderr, msg+"\n", args...)
+    os.Exit(1)
+}
+
+func HandleRequest(ctx context.Context) (error) {
+	var bucket = os.Getenv("imageBucket")
+	fmt.Println(bucket)
+
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+	
+	// Create S3 service client
+	svc := s3.New(sess)
+
+
 	// reads the directory given below
-	photos, err := ioutil.ReadDir("./input")
+	photos, err := ioutil.ReadDir("input/")
 	if err != nil {
 		fmt.Println("error reading directory")
 		log.Fatal(err)
@@ -24,7 +48,7 @@ func main() {
 	for _, f := range photos {
 
 		// open photo
-		file, err := os.Open("./input/"+f.Name())
+		file, err := os.Open("input/"+f.Name())
 		if err != nil {
 			fmt.Println("error opening photo and saving it as a file")
 			log.Fatal(err)
@@ -41,7 +65,7 @@ func main() {
 		// resize to width 2436 using Lanczos resampling and preserve aspect ratio
 		m := resize.Resize(2436, 0, img, resize.Lanczos3)
 
-		out, err := os.Create("./output/"+f.Name())
+		out, err := os.Create("output/"+f.Name())
 		if err != nil {
 			fmt.Println("error creating outputfile")
 			log.Fatal(err)
@@ -52,4 +76,5 @@ func main() {
 		jpeg.Encode(out, m, nil)
 		fmt.Println(f.Name() + " Done!")
 	}
+	return nil
 }
